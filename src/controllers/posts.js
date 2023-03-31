4c144072ce4e79c6a74ecb958c64a4121080bd7d
{its-ani} <{guptaanirudh712@gmail.com}>
2023-04-01 04:44:25 +0530

    Updates

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -30,10 +30,10 @@ async function task(){
     // console.log(await createNewPost(1, "this is sample post", "Body of the 1st posts goes here")),
     // console.log(await createNewPost(2, "Second  sample post", "Body of the 2nd posts goes here")),
     // console.log(await createNewPost(3, "Third is sample post", "Body of the 3rd posts goes here"))
-    const posts = await findAllPosts()
-    for(let p of posts){
-        console.log(`${p.title}\n author: ${p.user.username} \n${p.body}\n=======\n`)
-    }
+    // const posts = await findAllPosts()
+    // for(let p of posts){
+    //     console.log(`${p.title}\n author: ${p.user.username} \n${p.body}\n=======\n`)
+    // }
 }
 
 task()
